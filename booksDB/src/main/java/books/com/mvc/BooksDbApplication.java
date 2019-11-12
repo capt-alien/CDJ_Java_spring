25019168412be740eb8dc10e7636d30369996b91
capt-alien <eric.botcher@presidio.edu>
2019-11-12 12:41:56 -0800

    commiting

---
+++ booksDB/src/main/java/books/com/mvc/BooksDbApplication.java
@@ -0,0 +1,13 @@
+package books.com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDbApplication.class, args);
+	}
+
+}
